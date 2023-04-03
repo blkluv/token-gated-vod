d5118426f125b9f69dfe7798c738af7eb2551166
Suhail Kakar <suhail.zahed@gmail.com>
2023-04-04 02:41:16 +0530

    clean up the files

--- client/livepeer/index.ts
+++
@@ -1,8 +0,0 @@
-import { createReactClient, studioProvider } from "@livepeer/react";
-import { betaStudioProvider } from "../../lib/livepeer";
-
-const client = createReactClient({
-  provider: betaStudioProvider(),
-});
-
-export default client;
