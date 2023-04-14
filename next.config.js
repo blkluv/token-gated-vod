45964db7189ff48375e6e13a73fad3f663c4fc07
The Wizard of Hahz <thewizardofhahz@King-Hahz.hsd1.ga.comcast.net>
2023-04-14 14:01:49 -0400

    updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const withTM = require("next-transpile-modules")(["lit-share-modal-v3"])
+const withTM = require("next-transpile-modules")(["lit-share-modal-v3"]);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withTM({
   reactStrictMode: true,
   swcMinify: true,
-})
+  images: {
+    domains: ["www.ppv.arvrtise.com"],
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
